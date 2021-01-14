c9aeac68591042a5bea6ae8bd3951b0b8e4c3428
Shiloli-r <45619599+Shiloli-r@users.noreply.github.com>
2021-01-14 09:08:50 +0300

    Added Photo Form, Form processing

---
+++ cropper/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Photo(models.Model):
+    file = models.ImageField()
+    description = models.CharField(max_length=250)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'photo'
+        verbose_name_plural = 'photos'
---
+++ cropper/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from .models import Photo
+from .forms import PhotoForm
+
+
+# Create your views here.
+def home(request):
+    photos = Photo.objects.all()
+    if request.method == 'POST':
+        form = PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    else:
+        form = PhotoForm()
+    return render(request, 'base.html')
